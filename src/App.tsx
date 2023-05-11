21690f2d915cefad8c3911ebd883c9400bcd38cb
Jacob Barta <jacob.barta25@gmail.com>
2023-05-10 22:21:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import MainPage from "./pages/MainPage";
-import {HashRouter, NavLink, Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, NavLink, Route, Router, Routes} from "react-router-dom";
 import SettingsPage from "./pages/SettingsPage";
 
 function App() {
 
-  return (
-      <HashRouter basename={'/'}>
-            <Routes>
-              {/*<Route index path={"/"} element={<MainPage />} />*/}
-              {/*<Route path="/5907-pit-display/settings" element={<SettingsPage />} />*/}
 
-                {/*<Route path='/5907-pit-display'> /!* put url base here and nest children routes *!/*/}
-                    <Route path='/' element={ <MainPage /> } />
+  return (
+      <HashRouter basename={`/`}>
+            <Routes>
+                    <Route path='' element={ <MainPage /> } />
                     <Route path='/settings' element={ <SettingsPage /> } />
-                {/*</Route>*/}
+
                 <Route path="/*" element={<NavLink to="/" />}  /> {/* navigate to default route if no url matched */}
             </Routes>
 
