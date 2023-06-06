4d37de38b47a328b41ed028ab19cb2be19a3da3c
Jacob Barta <jacob.barta25@gmail.com>
2023-06-06 12:08:47 -0400

    dim the colors a bit and make the win pred not so gross

---
+++ src/components/advantage-bar/AdvantageBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AdvantageBar.css"
+
+function AdvantageBar(props: any) {
+    return(
+        <div className={"bar-container"}>
+        </div>
+    )
+}
+
+export default AdvantageBar;
